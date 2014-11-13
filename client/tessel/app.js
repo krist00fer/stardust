f9bd5718047d2d3ad89c97b8df9a14c0a9503a67
Kristofer Liljeblad <kristofer.liljeblad>
2014-11-13 19:32:28 +0100

    working

--- client/tessel/app.js
+++ client/tessel/app.js
@@ -1,13 +1,22 @@
 var tessel = require('tessel');
+var https = require('https');
 var ambientlib = require('ambient-attx4');
 var ambient = ambientlib.use(tessel.port['A']);
+
+var namespace = 'stardust';
+var hubname = 'stardust';
+var deviceName = 'TM-00-04-f000da30-0061473d-36582586'; // tessel.deviceId();
+console.log('deviceName:', deviceName);
+
+// SharedAccessSignature sr=https%3A%2F%2Fstardust.servicebus.windows.net%2Fstardust%2Fpublishers%2FTM-00-04-f000da30-0061473d-36582586%2Fmessages&sig=J5UuDPHYsZ7F%2FYMEyvBSBnS9WOY5KisR78YtdTu7rqk%3D&se=1417774602&skn=RootManageSharedAccessKey
+var sas = 'SharedAccessSignature sr=https%3A%2F%2Fstardust.servicebus.windows.net%2Fstardust%2Fpublishers%2FTM-00-04-f000da30-0061473d-36582586%2Fmessages&sig=WFPkJvHedoWgAPUnCBmeKytwtA7s26qkTwq4Ku%2FJ5W0%3D&se=1416766359&skn=RootManageSharedAccessKey';
 
 var samples = [];
 
 ambient.on('ready', function () {
 
   setImmediate(sampleSoundLevel);
-  setTimeout(sendToCloud, 1000);
+  setTimeout(sendToCloud, 500);
 
 });
 
@@ -26,12 +35,23 @@ function sendToCloud() {
   var noSamples = samples.length;
   var sum = samples.reduce(function(a, b) { return a + b });
   var avg = sum / samples.length;
+  var jsonAvg = JSON.stringify(avg);
 
   samples.length = 0;
 
   console.log('Avg sound level:', avg, 'Samples:', noSamples);
 
-  setTimeout(sendToCloud, 1000);
+  sendToEventHub(namespace, hubname, deviceName, sas, jsonAvg, 'application/json', function(error, result) {
+    if (!error) {
+      console.log('INFO Message sent\n');
+    } else {
+      console.log('ERROR Could not send data to event hub');
+      console.log(error);
+    }
+
+    setTimeout(sendToCloud, 500);
+
+  });
 }
 
 function sendToEventHub(namespace, hubname, deviceName, sas, content, contentType, callback) {
@@ -50,19 +70,28 @@ function sendToEventHub(namespace, hubname, deviceName, sas, content, contentTyp
     }
   };
 
+  console.log('INFO Sending data to Event Hub');
   var req = https.request(options, function(res) {
-  var body = '';
+    var body = '';
 
-  console.log('INFO HTTP StatusCode:', res.statusCode);
+    console.log('INFO HTTP StatusCode:', res.statusCode);
 
-  res.on('data', function(data) {
-    body += data;
+    res.on('data', function(data) {
+      body += data;
+    });
+
+    res.on('end', function() {
+      callback(null, body);
+    });
   });
 
-  res.on('end', function() {
-    callback(null, body);
+  req.on('error', function(error) {
+    callback(error, null);
   });
-  });
+
+  req.write(content);
+  req.end();
+
 }
 
 
@@ -70,39 +99,39 @@ function sendToEventHub(namespace, hubname, deviceName, sas, content, contentTyp
 
 
 
-***
+// ***
 
-// Payload to send
-var payload = '{\"Temperature\":\"37.0\",\"Humidity\":\"0.4\"}';
+// // Payload to send
+// var payload = '{\"Temperature\":\"37.0\",\"Humidity\":\"0.4\"}';
 
  
-// Send the request to the Event Hub
-var options = {
-  hostname: namespace + '.servicebus.windows.net',
-  port: 443,
-  path: '/' + hubname + '/publishers/' + deviceName + '/messages',
-  method: 'POST',
-  headers: {
-    'Authorization': createdSAS,
-    'Content-Length': payload.length,
-    'Content-Type': 'application/atom+xml;type=entry;charset=utf-8'
-  }
-};
+// // Send the request to the Event Hub
+// var options = {
+//   hostname: namespace + '.servicebus.windows.net',
+//   port: 443,
+//   path: '/' + hubname + '/publishers/' + deviceName + '/messages',
+//   method: 'POST',
+//   headers: {
+//     'Authorization': createdSAS,
+//     'Content-Length': payload.length,
+//     'Content-Type': 'application/atom+xml;type=entry;charset=utf-8'
+//   }
+// };
 
-var req = https.request(options, function(res) {
-  console.log('----------------------------------------------------------------------');
-  console.log("statusCode: ", res.statusCode);
-  console.log('----------------------------------------------------------------------');
-  console.log('');
-  res.on('data', function(d) {
-    process.stdout.write(d);
-  });
-});
+// var req = https.request(options, function(res) {
+//   console.log('----------------------------------------------------------------------');
+//   console.log("statusCode: ", res.statusCode);
+//   console.log('----------------------------------------------------------------------');
+//   console.log('');
+//   res.on('data', function(d) {
+//     process.stdout.write(d);
+//   });
+// });
  
-req.on('error', function(e) {
-  console.log('error');
-  console.error(e);
-});
+// req.on('error', function(e) {
+//   console.log('error');
+//   console.error(e);
+// });
  
-req.write(payload);
-req.end();
+// req.write(payload);
+// req.end();
